06e83d38d6a12c912be4769d104b6ab071fc4e6b
Darrian-Lambert <123426807+Darrian-Lambert@users.noreply.github.com>
2023-01-23 18:44:28 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace homeworkOne;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5;
+        double y = 3.3;
+        double z;
+
+        z = (4*x*x)+(3*y);
+
+        Console.WriteLine("X = 2.5, Y = 3.3");
+        Console.WriteLine("The value of Z is: " + z);
+
+    }
+}
